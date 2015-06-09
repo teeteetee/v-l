02199b73811ba951cbde0dbfd3a496058622ba0c
teeteetee <proto@protos-MacBook-Pro.local>
2015-06-09 09:26:27 -0700

    --

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ app.get('/',function(req,res) {
   res.render('index');
 });
 
+app.get('/2',function(req,res){
+  res.render('index2');
+});
+
 app.get('/submit',function(req,res){
   res.render('submit');
 });
