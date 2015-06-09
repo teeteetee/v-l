52e9aac1239c52ad528845f40437b282ce710006
teeteetee <proto@protos-MacBook-Pro.local>
2015-06-09 00:40:49 -0700

    Init

---
+++ app.js
@@ -0,0 +1,362 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sessions = require('client-sessions');
+var Cookies = require('cookies');
+var bcrypt = require('bcrypt');
+
+var mongo = require('mongodb');
+var db = require('monk')('localhost/tav')
+  , users = db.get('users'),insidemsg = db.get('insidemsg'),friends = db.get('friends');
+// POSTS and OBJECTS BELONGS TO MALESHIN PROJECT DELETE WHEN PUSHING TOPANDVIEWS TO PRODUCTION
+var fs = require('fs-extra');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+//app.use(require('connect').bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.compress());
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2540000000 }));
+app.use(sessions({
+  cookieName: 'session',
+  secret:'2342kjhkj2h3i2uh32j3hk2jDKLKSl23kh42u3ih4',
+  duration:4320 * 60 *1000,
+  activeduration:1440 * 60 * 1000,
+  cookie: {
+    path:'/',
+  httpOnly: true,
+  domain:'.xxxxxxxxxxxx.xxx'
+  }
+}));
+
+//SUBDOMAIN MAGIC 
+
+app.get('*',function(req,res,next){
+  if (req.ip === '188.226.189.180') {
+    console.log("c'est moi "+req.headers.host);
+    next();
+  } 
+  else{
+    next();
+  }
+});
+
+app.get('*', function(req,res,next) {  
+
+   if(req.headers.host === 'm.xxxxxxxxxxxx.xxx')  //if it's a sub-domain
+   {req.url = '/m' + req.url; 
+    console.log(req.url); //append some text yourself
+     next();}
+  else{
+   console.log('-------------- REQUEST --------------')
+   console.log('User-Agent: ' + req.headers['user-agent']);
+   console.log('URL: '+req.url);
+   console.log(req.ip);
+    next();}
+   });
+
+
+ 
+
+app.get('/',function(req,res) {
+  console.log('----- ON / ROUTE:'+JSON.stringify(req.session));
+  var userAgent=req.headers['user-agent'];
+  var uacheck = userAgent.indexOf("iPhone") != -1 ;
+});
+ 
+app.get('/dropplaces',function(req,res){
+  if(req.ip === '188.226.189.180' || req.session.sKK76d === 'porC6S78x0XZP1b2p08zGlq')
+    {places.remove({},function(err,done){
+        if(err)
+        {
+          res.send('98');
+        }
+        else {
+          res.send('SUCCESS');
+        }
+        });}
+  else {
+    res.redirect('http://yandex.ru');
+  }
+  });
+
+function messagescount () {
+  insidemsg.count({},function(err,c){
+    if(err) {
+      return 0;
+    }
+    else {
+      return c;
+    }
+  });
+}
+
+function getmessages () {
+  insidemsg.find({},function(err,doc){
+    if (err)
+    {
+      return 0;
+    }
+    else {
+      return doc;
+    }
+  });
+}
+
+app.get('/admax',function(req,res){
+  console.log("CHECKING COOKIES: "+JSON.stringify(req.session));
+  var lguser={};
+   if(req.session.sKK76d === 'porC6S78x0XZP1b2p08zGlq')
+   {
+   users.count({},function(err,c){
+    if (err)
+    {
+      res.send('DB ERR')
+    }
+  else {
+    friends.count({},function(err,d){
+      if(err)
+      {
+         res.send('DB ERR');
+      }
+      else {
+        if(messagescount)
+    {
+       var messages = getmessages;
+      res.render('admin',{'users':c,'friends':d,'doc':messages});
+     }
+     else {
+      res.render('adminzeromsg',{'users':c,'friends':d});
+     }
+      }
+
+    });
+
+  }
+  });
+  
+}
+   else {
+   res.render('adminauth');
+ }
+
+});
+
+app.post('/admax',function(req,res){
+  var pas = 'christ';
+  var log = 'jesus';
+  var vpas = req.body.vpas;
+  var vlog = req.body.vlog;
+  if(pas === vpas && log === vlog) {
+    req.session.sKK76d = 'porC6S78x0XZP1b2p08zGlq';
+    res.redirect('http://xxxxxxxxxxxx.xxx/admax');
+  }
+  else {
+    res.render('adminauth');
+  }
+});
+
+app.get('/admin/userlist',function(req,res){
+  if(req.ip === '188.226.189.180'  || req.session.sKK76d === 'porC6S78x0XZP1b2p08zGlq')
+  {
+    users.find({},function(err,doc){
+    if(err)
+    {
+      res.send('DB ERR')
+    }
+    else {
+      if(doc.length>0)
+      {
+         res.render('userlist',{'doc':doc});
+      }
+      else{
+         res.send('NO PLACES - EMPTY DB');
+      }
+    }
+  });
+  }
+  else{
+    res.redirect('http://ya.ru');
+  }
+});
+
+app.get('/admin/friendlist',function(req,res){
+  if(req.ip === '188.226.189.180'  || req.session.sKK76d === 'porC6S78x0XZP1b2p08zGlq')
+  {
+    friends.find({},function(err,doc){
+    if(err)
+    {
+      res.send('DB ERR')
+    }
+    else {
+      if(doc.length>0)
+      {  
+         console.log(doc);
+         res.render('friendlist',{'doc':doc});
+      }
+      else{
+         res.send('NO friends - EMPTY DB');
+      }
+    }
+  });
+  }
+  else{
+    res.redirect('http://ya.ru');
+  }
+});
+
+app.post('/drop/users',function(req,res){
+  if(req.ip === '188.226.189.180' || req.session.sKK76d === 'porC6S78x0XZP1b2p08zGlq')
+  {
+    var pp = 'secureshit';
+     if(req.body.p ===  pp)
+     {users.remove({});
+      friends.remove({});
+     console.log('USERS DB DROPPED FROM '+ req.ip);
+     req.session.reset();
+     res.redirect('http://xxxxxxxxxxxx.xxx/admax');}
+    else {
+      res.redirect('http://xxxxxxxxxxxx.xxx');
+    }
+  }
+  else {
+    res.redirect('http://xxxxxxxxxxxx.xxx');
+  }
+});
+
+app.post('/admin/1/:uid',function(req,res){
+  var pas = req.body.uu;
+  if (pas != 'withoutthesecurity') {
+    res.redirect('http://recentones.com');
+  }
+  else 
+  {var vuid = parseInt(req.params.uid);
+    var ms={};
+    ms.trouble=1;
+    ms.mtext = 'db';
+    users.remove({uid:vuid},function(err,done){
+      if(err){
+        res.send(ms);
+      }
+      else {
+        friends.remove({uid:vuid},function(err,done){
+      if(err){
+        res.send(ms);
+      }
+      else {
+        ms.trouble=0;
+        res.send(ms);
+      }
+    });
+      }
+    });}
+});
+
+app.post('/admin/insidemsg/remove',function(req,res){
+  console.log('removing a message');
+  var vmid = parseInt(req.body.mid);
+  var pas = req.body.pas;
+  if (pas != 'withoutthesecurity' || !vmid) {
+    res.redirect('http://recentones.com');
+  }
+  else 
+  { var ms={};
+    ms.trouble=1;
+    ms.mtext = 'db';
+    insidemsg.remove({mid:vmid},function(err,done){
+      if(err){
+        res.send(ms);
+      }
+      else {
+        ms.trouble=0;
+        res.send(ms);
+      }
+    });
+  }
+
+});
+
+app.post('/admin/insidemsg',function(req,res){
+  console.log('creating message;');
+  var vheading = req.body.heading;
+  var vtextbody = req.body.textbody;
+  var d = new Date();
+  var vday = d.getDate().toString();
+  var vmonth = d.getMonth()+1;
+  vmonth = vmonth.toString();
+  var vyear = d.getUTCFullYear().toString();
+  console.log('beginning');
+  if (vday.length===1){
+         vday='0'+vday;
+       }
+  if (vmonth.length===1){
+         vmonth='0'+vmonth;
+       }
+  var vregdateint= vyear+vmonth+vday;
+  vregdateint = parseInt(vregdateint);
+  var ms = {};
+  ms.trouble=1;
+  ms.mtext = 'db';
+  console.log('middle');
+  insidemsg.find({},{limit:1,sort:{mid:-1}},function(err,doc){
+    if(err)
+    {
+      //clap your hands
+      res.send(ms);
+    }
+   else {
+    if(doc.length>0){
+      console.log('end');
+         var newid = doc[0].mid;
+         newid++;
+         console.log(newid);
+         insidemsg.insert({mid: newid,heading: vheading,textbody: vtextbody,regdateint: vregdateint,regdate:{day:vday,month:vmonth,year:vyear}});
+      ms.trouble=0;
+      res.send(ms);
+       }
+       else {
+         insidemsg.insert({mid: 1,heading: vheading,textbody: vtextbody,regdateint: vregdateint,regdate:{day:vday,month:vmonth,year:vyear}});
+         ms.trouble=0;
+      res.send(ms);
+       }
+   }
+  });
+});
+// production error handler
+// no stacktraces leaked to user
+app.use(function(req, res) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.render('404');
+});
+
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+app.listen(80,'xxx.xxx.xxx.xxx');
+// zero downtime with naught
+if (process.send) process.send('online');
+process.on('message', function(message) {
+  if (message === 'shutdown') {
+    //Do whatever you need to do before shutdown (cleanup, saving stuff, etc.)
+    process.exit(0);
+  }
+});
